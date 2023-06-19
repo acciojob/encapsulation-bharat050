e08a10716d0bfadfe369f0bcdd1f9929670ed3fe
Bharat <aimhigh050@gmail.com>
2023-06-20 01:36:20 +0530

    First Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly read = new RWOnly();
+        read.setName("bharat");
+        System.out.println(read.getName()); // attribute "name" has a private access
+    }
 }
